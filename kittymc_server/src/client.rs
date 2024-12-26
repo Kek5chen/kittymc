764929bc4f08e2f660a802cbb0d814dee95df435
IT0NA31 <ITnullNA31@gmail.com>
2024-12-26 21:41:50 +0100

    feat: handle client communication

--- kittymc_server/src/client.rs
+++ kittymc_server/src/client.rs
@@ -1,10 +1,15 @@
+use std::io::ErrorKind;
 use std::net::SocketAddr;
+use tokio::io::AsyncReadExt;
 use tokio::net::{TcpListener, TcpStream};
 use kittymc_lib::error::KittyMCError;
+use kittymc_lib::packets::Packet;
+use kittymc_lib::subtypes::state::State;
 
 pub struct Client {
     socket: TcpStream,
     addr: SocketAddr,
+    current_state: State,
 }
 
 impl Client {
@@ -15,11 +20,12 @@ impl Client {
 
         Ok(Client {
             socket,
-            addr
+            addr,
+            current_state: State::Handshake,
         })
     }
 
-    pub async fn run(self) {
+    pub async fn run(mut self) {
         tokio::spawn(async move {
             match self.client_loop().await {
                 Err(e) => eprintln!("Fatal error in client {}: {e}", self.addr),
@@ -28,8 +34,36 @@ impl Client {
         });
     }
 
-    async fn client_loop(&self) -> Result<(), KittyMCError> {
+    async fn client_loop(&mut self) -> Result<(), KittyMCError> {
         loop {
+            match self.socket.readable().await {
+                Ok(_) => {}
+                Err(ref e) if e.kind() == ErrorKind::WouldBlock => continue,
+                Err(e) => Err(e)?,
+            }
+            let mut buffer = vec![0u8; 2048];
+            let mut n = match self.socket.read(&mut buffer).await {
+                Ok(0) => {
+                    // The other side closed the connection
+                    return Ok(());
+                }
+                Ok(n) => n,
+                Err(e) => Err(e)?,
+            };
+            let packet = Packet::deserialize_packet(self.current_state, &buffer);
+            match &packet {
+                Ok((n, packet)) => match packet {
+                    Packet::Handshake(handshake) => {
+                        if handshake.protocol_version != 47 {
+                            return Ok(());
+                        }
+                        self.current_state = handshake.next_state;
+                    }
+                    Packet::LoginStart(_) => {}
+                }
+                Err(e) => continue,
+            }
+            println!("buffer: {:?}", packet);
         }
     }
 }
