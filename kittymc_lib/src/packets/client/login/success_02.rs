d3fc0ed5437ed2dea5617ac30913bda928ae1bf7
Kek5chen <52585984+Kek5chen@users.noreply.github.com>
2025-01-03 19:55:17 +0100

    fix: offline mode uuid generation

--- kittymc_lib/src/packets/client/login/success_02.rs
+++ kittymc_lib/src/packets/client/login/success_02.rs
@@ -16,7 +16,7 @@ impl LoginSuccessPacket {
             return Err(KittyMCError::TooMuchData(name.len(), 16));
         }
 
-        let md5 = md5::compute(name);
+        let md5 = md5::compute(format!("OfflinePlayer:{name}"));
 
         Ok(Builder::from_md5_bytes(md5.0).into_uuid())
     }
@@ -31,12 +31,19 @@ impl LoginSuccessPacket {
     }
 }
 
+#[test]
+fn test_cracked_uuid() {
+    assert_eq!(LoginSuccessPacket::generate_cracked_uuid("will_owo").unwrap(), Uuid::from_str("0e22d127-3477-35f9-a65a-6fb3611c78fb").unwrap());
+    assert_eq!(LoginSuccessPacket::generate_cracked_uuid("meow").unwrap(), Uuid::from_str("dadfb5ef-c239-3cb3-b316-aec3a76dbc71").unwrap());
+    assert_eq!(LoginSuccessPacket::generate_cracked_uuid("IT0NA31").unwrap(), Uuid::from_str("fe86cee2-9d18-3100-bc41-6740712ec780").unwrap());
+}
+
+
 impl SerializablePacket for LoginSuccessPacket {
     fn serialize(&self) -> Vec<u8> {
         let mut packet = vec![];
 
         write_length_prefixed_string(&mut packet, &self.uuid.hyphenated().to_string());
-
         write_length_prefixed_string(&mut packet, &self.username);
 
         wrap_packet(&mut packet, 2);
