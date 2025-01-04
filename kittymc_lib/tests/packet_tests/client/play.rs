b19d87ade5d2411f032d7d02d62b4759255f7b7e
Kek5chen <52585984+Kek5chen@users.noreply.github.com>
2025-01-04 16:48:28 +0100

    tests: fix tests

--- kittymc_lib/tests/packet_tests/client/play.rs
+++ kittymc_lib/tests/packet_tests/client/play.rs
@@ -13,7 +13,12 @@ fn test_03_set_compression() {
 
 #[test]
 fn test_02_login_success() {
-    let raw_packet = b"0\x00\x02$0e22d127-3477-35f9-a65a-6fb3611c78fb\x08will_owo";
-    let packet = LoginSuccessPacket::deserialize(&raw_packet[3..]).unwrap();
+    let _actual_real_raw_packet = b"0\x00\x02$0e22d127-3477-35f9-a65a-6fb3611c78fb\x08will_owo";
+    let raw_packet = b"/\x02$0e22d127-3477-35f9-a65a-6fb3611c78fb\x08will_owo";
+    // The difference here is that the actual packet serialized its varints badly. It adds an extra 0 after the packet length
+    // The raw packet is a slightly modified version since kittymc does not have this encoding error
+    // clients accept it fine without the extra 0
+
+    let packet = LoginSuccessPacket::deserialize(&raw_packet[2..]).unwrap();
     assert_eq!(&packet.1.serialize(), raw_packet) // Here the implementation of integer-encodings varint encode is different than the minecraft one
 }
